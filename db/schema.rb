e669d4a17df75cdc3bd2f57cfaac7f0db65a4615
cyberdavinci <ebrimaa2ray@gmail.com>
2023-12-02 21:29:24 +0000

    switched to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_27_101848) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_26_212009) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blog_posts", force: :cascade do |t|
     t.string "title"
     t.text "body"
+    t.string "author"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "author"
   end
 
   create_table "users", force: :cascade do |t|
